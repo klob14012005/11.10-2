e6ffcbd81e0dfb639e02bb873ccae5905c2edf25
Lobachev Konstantin <klob14012005@gmail.com>
2021-10-11 18:16:52 +0300

    Добавьте файлы проекта.

---
+++ 11.10-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11._10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
